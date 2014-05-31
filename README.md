4dcf8e22e82f2e2307018100b0d7875cd74aad79
chrisrw <chrisrw@hungryfishmedia.com>
2014-05-31 17:54:36 -0400

    Some partial view scripts added - resume still needs some attention - namely the resources (pdf/word)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Rewriting personal website with the help of Yeoman and Angular.js
 
-More to come here...
+Added GA tracking but may want to add custom events for people viewing resume and portfolio.
+Also want to set it to not fire during local dev - commented out send of beacon for now.
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,9 +7,13 @@ angular.module('crwApp')
       'AngularJS',
       'Karma'
     ];
-  });
+  })
 
-angular.module('crwApp')
+  .controller('ngBindHtmlCtrl', ['$scope', function ngBindHtmlCtrl($scope) {
+      $scope.appHTML =
+         'I am an <code>HTML</code>string with <a href="#">links!</a> and other <em>stuff</em>';
+    }])
+
   .controller('CrwCtrl', function () {
 
     this.tab = 1;
@@ -25,11 +29,11 @@ angular.module('crwApp')
     // Markup in the form of properties - might revisit on the best way to do this
     this.main = {
         about: {
-            paragraphs: {
-                one: 'I\'m a web developer, cook, musician & amateur brewer of beer and kombucha.  I live in Brooklyn, NY. and when not pursuing these ventures I enjoy cycling, reading, traveling, kickboxing and laughing at anything funny.',
-                two: 'Update SinglePlatform.  I work at <a href="http://www.nutraclick.com" target="_blank"><strong>NutraClick</strong></a> as a full stack developer tackling things small, large and extraordinary.  The challenges encountered so far have strengthened my skills and solidified my commitment to becoming the most well rounded developer I can be.  I\'ve worked for a web development and design company located in New York - <a href="http://www.zooluweb.com" title="website design shop"><strong>Zoolu Web Solutions</strong></a> as well as a financial consulting firm <a href="http://www.truebridge.com"><strong>Truebridge</strong></a> expanding on a browser based CRM.  These jobs complemented my learning while completing my CS degree from BU and playing as much music as I could.',
-                three: 'XXX - More updates'
-              }
+            paragraphs: [
+                'I\'m a web developer, cook, musician & amateur brewer of beer and kombucha.  I live in Brooklyn, NY. and when not pursuing these ventures I enjoy cycling, reading, traveling, kickboxing and laughing at anything funny.',
+                '<<<Update SinglePlatform>>>.  I work at &lt;a href="http://www.nutraclick.com" target="_blank"&gt;<strong>NutraClick</strong></a> as a full stack developer tackling things small, large and extraordinary.  The challenges encountered so far have strengthened my skills and solidified my commitment to becoming the most well rounded developer I can be.  I\'ve worked for a web development and design company located in New York - <a href="http://www.zooluweb.com" title="website design shop"><strong>Zoolu Web Solutions</strong></a> as well as a financial consulting firm <a href="http://www.truebridge.com"><strong>Truebridge</strong></a> expanding on a browser based CRM.  These jobs complemented my learning while completing my CS degree from BU and playing as much music as I could.',
+                'XXX - More updates'
+              ]
             }
           };
   });
